a06b150d990f9bea17f710475a239e95e96a0c78
Anıl Çetin <75864563+llnallna@users.noreply.github.com>
2024-11-29 22:13:11 +0300

    JQuery

---
+++ Gun40/script.js
@@ -0,0 +1,11 @@
+// document.getElementById("baslik").textContent = "JQuery Öğreniyorum";
+// document.getElementById("baslik").style.border = "1px solid black";
+// document.getElementById("baslik").setAttribute("title", "bu bir başlıktır");
+
+//         // 4- başlıktan sonra hr ekle
+// let hr = document.createElement("hr");
+// document.getElementById("baslik").after(hr);
+
+// document.querySelectorAll("li").forEach((li, indeks) => {
+//   li.textContent += " :) -" + indeks;
+// });
